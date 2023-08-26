f22a43fd0813c337cbc0107c994e6888ed8de59a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-25 21:17:53 -0300

    atributos do Salgado

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
@@ -1,5 +1,12 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
+import java.math.BigDecimal;
+
 public class Salgado {
 
+	private String nome;
+	private String descricao;
+	private String ingrediente;
+	private BigDecimal preco;
+	private Integer quantidade;
 }
