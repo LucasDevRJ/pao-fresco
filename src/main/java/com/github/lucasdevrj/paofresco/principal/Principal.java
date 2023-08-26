6322943791ee1fcb76b4bf4a7662183558650d73
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-26 19:00:16 -0300

    adição do menu interativo

--- src/main/java/com/github/lucasdevrj/paofresco/principal/Principal.java
+++ src/main/java/com/github/lucasdevrj/paofresco/principal/Principal.java
@@ -1,5 +1,15 @@
 package com.github.lucasdevrj.paofresco.principal;
 
-public class Principal {
+import java.util.Scanner;
 
+public class Principal {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("--------------------|MENU PRINCIPAL|--------------------");
+		System.out.println("1 - Menu Salgados");
+		System.out.println("2 - Menu Refrescos");
+		System.out.print("Digite a opção desejada: ");
+		int opcao = entrada.nextInt();
+	}
 }
