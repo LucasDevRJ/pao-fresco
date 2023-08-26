296ea70fdc0838f38218d78a7791182a122e5c52
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-25 21:20:21 -0300

    gerando construtores para o Salgado

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
@@ -4,9 +4,23 @@ import java.math.BigDecimal;
 
 public class Salgado {
 
+	private Integer id;
 	private String nome;
 	private String descricao;
 	private String ingrediente;
 	private BigDecimal preco;
 	private Integer quantidade;
+	
+	public Salgado(String nome, String descricao, String ingrediente, BigDecimal preco, Integer quantidade) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.ingrediente = ingrediente;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	
+	public Salgado() {
+		
+	}
+	
 }
