0d21753b5513441c15240ea368a615f5e0780c81
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-27 18:51:25 -0300

    criação dos atributos para a classe da Lanchonete

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Lanchonete.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Lanchonete.java
@@ -1,5 +1,10 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
+import java.math.BigDecimal;
+
 public class Lanchonete {
 
+	private final String nome = "Pão Fresco";
+	private String endereco;
+	private BigDecimal receita;
 }
