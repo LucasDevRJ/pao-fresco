a899ecf463149d3bbbcf27de93d22c613218e1bc
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-27 18:53:26 -0300

    anotações na classe

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Lanchonete.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Lanchonete.java
@@ -2,8 +2,18 @@ package com.github.lucasdevrj.paofresco.modelos;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "lanchonete")
 public class Lanchonete {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private final String nome = "Pão Fresco";
 	private String endereco;
 	private BigDecimal receita;
