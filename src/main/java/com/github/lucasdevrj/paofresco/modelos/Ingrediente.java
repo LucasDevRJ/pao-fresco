f78ad413af1c3ae958f599f269101038ae8bafd9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-28 19:07:41 -0300

    criando método para comprar ingrediente

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Ingrediente.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Ingrediente.java
@@ -1,7 +1,26 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ingredientes")
 public class Ingrediente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String nome;
 	private Double quantidadeGramas;
+	
+	public Ingrediente(String nome, Double quantidadeGramas) {
+		this.nome = nome;
+		this.quantidadeGramas = quantidadeGramas;
+	}
+	
+	public Ingrediente() {
+		
+	}
 }
--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Mercado.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Mercado.java
@@ -1,7 +1,21 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
+import java.util.Scanner;
+
 public class Mercado {
 
 	private String nome;
 	
+	public void comprar() {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o nome do ingrediente que deseja comprar: ");
+		String nome = entrada.nextLine();
+		
+		System.out.print("Digite a quantidade(gramas) do ingrediente que deseja comprar: ");
+		double gramas = entrada.nextDouble();
+		
+		Ingrediente ingrediente = new Ingrediente(nome, gramas);
+		
+	}
 }
