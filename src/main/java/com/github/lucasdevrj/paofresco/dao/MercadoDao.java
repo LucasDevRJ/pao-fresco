14e34605355a6d54380d2bf58760b7f04ace6934
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-29 18:57:37 -0300

    método para cadastrar mercado

--- src/main/java/com/github/lucasdevrj/paofresco/dao/MercadoDao.java
+++ src/main/java/com/github/lucasdevrj/paofresco/dao/MercadoDao.java
@@ -2,11 +2,17 @@ package com.github.lucasdevrj.paofresco.dao;
 
 import javax.persistence.EntityManager;
 
+import com.github.lucasdevrj.paofresco.modelos.Mercado;
+
 public class MercadoDao {
 
 	private EntityManager em;
 	
 	public MercadoDao(EntityManager em) {
 		this.em = em;
+	}
+	
+	public void cadastrar(Mercado mercado) {
+		this.em.persist(mercado);
 	}
 }
