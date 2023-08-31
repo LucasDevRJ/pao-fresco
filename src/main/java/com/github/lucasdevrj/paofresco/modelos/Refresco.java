072f57b59d4b8489ec486e4ad1beecf92bcd8f9d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-31 20:50:14 -0300

    herança entre Refresco e Alimento

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Refresco.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Refresco.java
@@ -1,11 +1,25 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Refresco {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "refrescos")
+public class Refresco extends Alimento {
 
 	private Double mililitros;
-	
 	private Double preco;
+	
+	public Refresco(String nome, List<Ingrediente> ingredientes, String descricao, Double preco, Double mililitros,
+			Double preco2) {
+		super(nome, ingredientes, descricao, preco);
+		this.mililitros = mililitros;
+		preco = preco2;
+	}
+	
+	public Refresco() {
+		
+	}
 }
