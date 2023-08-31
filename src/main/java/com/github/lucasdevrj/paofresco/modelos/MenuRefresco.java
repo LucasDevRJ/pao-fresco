8b9e80c1fa160f1cd2847043bd6f5873ccee815d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-31 20:27:23 -0300

    método para exibir menu

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/MenuRefresco.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/MenuRefresco.java
@@ -1,5 +1,43 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
+import java.util.Scanner;
+
 public class MenuRefresco {
+
+	private Scanner entrada = new Scanner(System.in);
+	
+	public void exibeMenu() {
+		System.out.println("--------------------|MENU REFRESCO|--------------------");
+		System.out.println("1 - Cadastrar Refresco");
+		System.out.println("2 - Pesquisar Refresco");
+		System.out.println("3 - Exibir Todos Refresco");
+		System.out.println("4 - Atualizar Refresco");
+		System.out.println("5 - Excluir Refresco");
+		System.out.print("Digite a opção desejada: ");
+		int opcao = entrada.nextInt();
+		
+		switch (opcao) {
+			case 1:
+				
+			break;
+			
+			case 2:
+				
+			break;
+			
+			case 3:
+				
+			break;
+			
+			case 4:
+				
+			break;
+			
+			case 5:
+				
+			break;
+		}
+		exibeMenu();
+	}
 
 }
