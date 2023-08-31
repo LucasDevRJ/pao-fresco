1389a079f23ac92e3c78173db92f52ae451acbe0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-31 20:36:02 -0300

    encapsulamento da classe mãe

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Alimento.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Alimento.java
@@ -1,5 +1,43 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
-public class Alimento {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "alimentos")
+public abstract class Alimento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private Double preco;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 }
--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
@@ -8,69 +8,15 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "salgados")
-public class Salgado {
+public class Salgado extends Alimento {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	private String nome;
-	private String descricao;
-	private Double preco;
 	private Double quantidadeGramas;
 	private Double pesoGramas;
-	
-	public Salgado(String nome, String descricao, Double preco, Double quantidadeGramas, Double pesoGramas) {
-		this.nome = nome;
-		this.descricao = descricao;
-		this.preco = preco;
-		this.quantidadeGramas = quantidadeGramas;
-		this.pesoGramas = pesoGramas;
-	}
-
-	public Salgado() {
-		
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	public Double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
-	
-	public Double getQuantidadeGramas() {
-		return quantidadeGramas;
-	}
-	
-	public void setQuantidadeGramas(Double quantidadeGramas) {
-		this.quantidadeGramas = quantidadeGramas;
-	}
-	
-	public Double getPesoGramas() {
-		return pesoGramas;
-	}
-
-	public void setPesoGramas(Double pesoGramas) {
-		this.pesoGramas = pesoGramas;
-	}
 
 	@Override
 	public String toString() {
 		return "|ID: " + this.id + "|Nome: " + this.nome + 
-		"|Descrição: " + this.descricao + "|Preço: R$" +
+		"|Descrição: " + super.descricao + "|Preço: R$" +
 		this.preco + "|Quantidade(kg): " + this.quantidadeGramas +
 		"|Peso(gramas): " + this.pesoGramas + "|";
 	}
