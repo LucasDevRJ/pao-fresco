a720f59a202d585dc915bfdfa82928c78751ac0a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-31 20:32:20 -0300

    criação da classe Alimento para ser a mãe

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Refresco.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Refresco.java
@@ -1,5 +1,12 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Refresco {
 
+	private String nome;
+	private Double mililitros;
+	private List<Ingrediente> ingredientes = new ArrayList<>();
+	private Double preco;
 }
