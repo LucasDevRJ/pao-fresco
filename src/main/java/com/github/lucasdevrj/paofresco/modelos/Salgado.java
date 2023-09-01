9663ca6eb8e8065b583c957199080dbf1814608c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-01 19:38:43 -0300

    refatoração do Salgado

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
@@ -9,39 +9,14 @@ import javax.persistence.Table;
 @Table(name = "salgados")
 public class Salgado extends Alimento {
 
-	private Double quantidadeGramas;
 	private Double pesoGramas;
-	
-	public Salgado(String nome, List<Ingrediente> ingredientes, String descricao, Double preco, Double quantidadeGramas,
-			Double pesoGramas) {
+
+	public Salgado(String nome, List<Ingrediente> ingredientes, String descricao, Double preco, Double pesoGramas) {
 		super(nome, ingredientes, descricao, preco);
-		this.quantidadeGramas = quantidadeGramas;
 		this.pesoGramas = pesoGramas;
 	}
-
+	
 	public Salgado() {
 		
-	}
-
-	public Double getQuantidadeGramas() {
-		return quantidadeGramas;
-	}
-
-	public void setQuantidadeGramas(Double quantidadeGramas) {
-		this.quantidadeGramas = quantidadeGramas;
-	}
-
-	public Double getPesoGramas() {
-		return pesoGramas;
-	}
-
-	public void setPesoGramas(Double pesoGramas) {
-		this.pesoGramas = pesoGramas;
-	}
-	
-	@Override
-	public String toString() {
-		return super.toString() + "|Gramas: " + this.quantidadeGramas +
-				"|Peso: " + this.pesoGramas;
 	}
 }
