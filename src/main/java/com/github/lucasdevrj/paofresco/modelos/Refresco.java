3ea5233c881ae2e79f2bea8f2d10423645a68afa
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-02 21:36:32 -0300

    gerando construtores e encapsulamento do Salgado

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Refresco.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Refresco.java
@@ -1,36 +1,83 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "refrescos")
 public class Refresco {
 
-	private Double mililitros;
-
-	public Refresco(String nome, String descricao, List<Ingrediente> ingredientes, Double preco, Double mililitros) {
-		super(nome, descricao, ingredientes, preco);
-		this.mililitros = mililitros;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private List<Ingrediente> ingredientes = new ArrayList<>();
+	private Double preco;
+	private Double pesoGramas;
+	
+	public Refresco(String nome, String descricao, List<Ingrediente> ingredientes, Double preco, Double pesoGramas) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.ingredientes = ingredientes;
+		this.preco = preco;
+		this.pesoGramas = pesoGramas;
 	}
 
-	public Refresco() {
-		
+	public Integer getId() {
+		return id;
 	}
 
-	public Double getMililitros() {
-		return mililitros;
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
-	public void setMililitros(Double mililitros) {
-		this.mililitros = mililitros;
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Ingrediente> getIngredientes() {
+		return ingredientes;
+	}
+
+	public void setIngredientes(List<Ingrediente> ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Double getPesoGramas() {
+		return pesoGramas;
+	}
+
+	public void setPesoGramas(Double pesoGramas) {
+		this.pesoGramas = pesoGramas;
 	}
 	
-	@Override
-	public String toString() {
-		return super.toString() + "|Mililitros(quantidade): " + this.mililitros;
-	}
 	
+
 }
--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Salgado.java
@@ -4,15 +4,78 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "salgados")
 public class Salgado {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String nome;
 	private String descricao;
 	private List<Ingrediente> ingredientes = new ArrayList<>();
 	private Double preco;
 	private Double pesoGramas;
+	
+	public Salgado(String nome, String descricao, List<Ingrediente> ingredientes, Double preco, Double pesoGramas) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.ingredientes = ingredientes;
+		this.preco = preco;
+		this.pesoGramas = pesoGramas;
+	}
+	
+	public Salgado() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Ingrediente> getIngredientes() {
+		return ingredientes;
+	}
+
+	public void setIngredientes(List<Ingrediente> ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Double getPesoGramas() {
+		return pesoGramas;
+	}
+
+	public void setPesoGramas(Double pesoGramas) {
+		this.pesoGramas = pesoGramas;
+	}
 }
