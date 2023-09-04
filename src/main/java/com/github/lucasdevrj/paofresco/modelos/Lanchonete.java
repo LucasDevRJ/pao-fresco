55abce8995429cd323819469ea3a95481b3f415f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-03 21:32:29 -0300

    adição de anotações

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Lanchonete.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Lanchonete.java
@@ -1,7 +1,17 @@
 package com.github.lucasdevrj.paofresco.modelos;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "lanchonetes")
 public class Lanchonete {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome = "Pão Fresco";
 	private String endereco;
