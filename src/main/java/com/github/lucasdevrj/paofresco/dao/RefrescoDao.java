7e0a0ae99206ee2a494d94965f23f889cbb7c3c4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-03 21:36:23 -0300

    contrutor

--- src/main/java/com/github/lucasdevrj/paofresco/dao/RefrescoDao.java
+++ src/main/java/com/github/lucasdevrj/paofresco/dao/RefrescoDao.java
@@ -1,5 +1,16 @@
 package com.github.lucasdevrj.paofresco.dao;
 
+import javax.persistence.EntityManager;
+
+import com.github.lucasdevrj.paofresco.util.JPAUtil;
+
 public class RefrescoDao {
 
+	private EntityManager em = JPAUtil.getEntityManager();
+	
+	public RefrescoDao(EntityManager em) {
+		this.em = em;
+	}
+	
+	
 }
