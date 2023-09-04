da7d2b4e3280e2cd7c1967e6b77cb22f5f3ef35f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-03 21:34:28 -0300

    encapsulando

--- src/main/java/com/github/lucasdevrj/paofresco/modelos/Lanchonete.java
+++ src/main/java/com/github/lucasdevrj/paofresco/modelos/Lanchonete.java
@@ -23,4 +23,20 @@ public class Lanchonete {
 	public Lanchonete() {
 		
 	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
 }
