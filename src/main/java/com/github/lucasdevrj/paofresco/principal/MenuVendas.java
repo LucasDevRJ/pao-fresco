e8b2ff667083525cb0ff967b4efbb1c321fc5105
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-09-07 20:37:08 -0300

    menu da classe de vendas criado

--- src/main/java/com/github/lucasdevrj/paofresco/principal/MenuVendas.java
+++ src/main/java/com/github/lucasdevrj/paofresco/principal/MenuVendas.java
@@ -1,5 +1,30 @@
 package com.github.lucasdevrj.paofresco.principal;
 
-public class MenuVendas {
+import java.util.Scanner;
 
+public class MenuVendas {
+	
+private static Scanner entrada = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		exibeMenu();
+	}
+	
+	public static void exibeMenu() {
+		System.out.println("--------------------|MENU VENDAS|--------------------");
+		System.out.println("1 - Vender");
+		System.out.println("2 - Sair");
+		System.out.print("Digite a opção desejada: ");
+		int opcao = entrada.nextInt();
+		
+		switch (opcao) {
+			case 1:
+				
+			break;
+			
+			case 2:
+				
+			break;
+		}
+	}
 }
